4c3fbf88cb6c055609141729fe3ee1a6d31328da
YaliW <wangyalijob@163.com>
2020-02-29 22:02:09 +0800

    feat:update paging hook

--- src/routers/Home/components/Paging.jsx
+++ src/routers/Home/components/Paging.jsx
@@ -1,109 +1,92 @@
-import React, {Component} from 'react'
+import React, { useState, useMemo } from 'react';
 
-class Paging extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            limit: 35,
-            offset: 0,
-            currPage: 1,
-        }
+function getTotalPages(a, b) {
+  return Math.ceil(a / b);
+}
+
+function getLeftEllipsis(param) {
+  return param > 4;
+}
+
+function getRightEllipsis(totalPlay, limit, currPage) {
+  return Math.ceil(totalPlay / limit) - currPage > 4;
+}
+
+function getCenterArr(totalPlay, limit, currPage) {
+  const result = [];
+  const totalPages = Math.ceil(totalPlay / limit);
+  const left = currPage > 5 ? (totalPages < currPage + 4 ? Math.ceil(totalPlay / limit) - 7 : currPage - 3) : 2;
+  const isEnough = Math.ceil(totalPlay / limit) > currPage + 3;
+  const right = isEnough ? Math.max(currPage + 3, left + 6) : Math.ceil(totalPlay / limit) - 1;
+  for (let index = left; index <= right; index++) {
+      result.push(index);
+  }
+  return result;
+}
+
+function Paging(props) {
+    const limit = 35;
+    const [ offset, setOffset ] = useState(0);
+    const [ currPage, setCurrPage ] = useState(1);
+    const totalPlay = props.data;
+
+    const totalPages = useMemo(()=> getTotalPages(totalPlay, limit), [totalPlay, limit]);
+    const leftEllipsis = useMemo(() => getLeftEllipsis(currPage), [currPage]);
+    const rightEllipsis = useMemo(() => getRightEllipsis(totalPlay, limit, currPage), [totalPlay, limit, currPage]);
+    const centerArr = useMemo(() => getCenterArr(totalPlay, limit, currPage), [totalPlay, limit, currPage]);
+
+    let leftEllipsisDom = null;
+    if (leftEllipsis) {
+        leftEllipsisDom = <span>...</span>
     }
-
-    get totalPages() {
-        const {limit} = this.state;
-        const totalPlay = this.props.data;
-        return Math.ceil(totalPlay / limit);
-    }
-
-    get leftEllipsis() {
-        return this.state.currPage > 4;
-    }
-
-    get rightEllipsis() {
-        const {limit, currPage} = this.state;
-        const totalPlay = this.props.data;
-        return Math.ceil(totalPlay / limit) - currPage > 4;
-    }
-
-    get centerArr() {
-        const {limit, currPage} = this.state;
-        const totalPlay = this.props.data;
-        const result = [];
-        const left = currPage > 5 ? (this.totalPages < currPage + 4 ? Math.ceil(totalPlay / limit) - 7 : currPage - 3) : 2;
-        const isEnough = Math.ceil(totalPlay / limit) > currPage + 3; const right = isEnough ? Math.max(currPage + 3, left + 6) : Math.ceil(totalPlay / limit) - 1;
-        for (let index = left; index <= right; index++) {
-            result.push(index);
-        }
-        return result;
-    }
-
-    render() {
-        const {currPage} = this.state;
-        let leftEllipsisDom = null;
-        if (this.leftEllipsis) {
-            leftEllipsisDom = <span>...</span>
-        }
-        const centerArrDom = this.centerArr.map((item) => {
-            return (
-                <span key={item}>
-                    <span className={`num-page ${currPage === item ? 'curr-page' : ''}`} onClick={() => this.clickPage(item)}>{item}</span >
-                </span>
-            )
-        })
-        let rightEllipsisDom = null;
-        if (this.rightEllipsis) {
-            rightEllipsisDom = <span>...</span>
-        }
+    const centerArrDom = centerArr.map((item) => {
         return (
-            <div className="paging-wrapper">
-                <div className="paging-container">
-                    <span className={`page prev-page ${currPage === 1 ? 'grey-prev-page' : ''}`} onClick={() => this.clickPrevPage()}>上一页</span >
-                    <span className={`num-page ${currPage === 1 ? 'curr-page' : ''}`} onClick={() => this.clickPage(1)}>1</span>
-                    {leftEllipsisDom}
-                    {centerArrDom}
-                    {rightEllipsisDom}
-                    <span className={`num-page ${currPage === this.totalPages ? 'curr-page' : ''}`} onClick={() => this.clickPage(this.totalPages)}>{this.totalPages}</span >
-                    <span className={`page next-page ${currPage === this.totalPages ? 'grey-next-page' : ''}`} onClick={() => this.clickNextPage()}>下一页</span >
-                </div>
-            </div>
+            <span key={item}>
+                <span className={`num-page ${currPage === item ? 'curr-page' : ''}`} onClick={() => clickPage(item)}>{item}</span >
+            </span>
         )
+    })
+    let rightEllipsisDom = null;
+    if (rightEllipsis) {
+        rightEllipsisDom = <span>...</span>
+    }
+    return (
+        <div className="paging-wrapper">
+            <div className="paging-container">
+                <span className={`page prev-page ${currPage === 1 ? 'grey-prev-page' : ''}`} onClick={() => clickPrevPage()}>上一页</span >
+                <span className={`num-page ${currPage === 1 ? 'curr-page' : ''}`} onClick={() => clickPage(1)}>1</span>
+                {leftEllipsisDom}
+                {centerArrDom}
+                {rightEllipsisDom}
+                <span className={`num-page ${currPage === totalPages ? 'curr-page' : ''}`} onClick={() => clickPage(totalPages)}>{totalPages}</span >
+                <span className={`page next-page ${currPage === totalPages ? 'grey-next-page' : ''}`} onClick={() => clickNextPage()}>下一页</span >
+            </div>
+        </div>
+    )
+
+    function clickPage(page) {
+      setCurrPage(page);
+      setOffset(limit * (page - 1));
+      handleClick();
     }
 
-    clickPage(page) {
-        const {limit } = this.state;
-        this.setState({
-            currPage: page,
-            offset: limit * (page - 1)
-        }, this.handleClick);
-        // this.$emit('clickPage', {value: this.offset});
+    function clickPrevPage() {
+      if (currPage > 1) {
+        setCurrPage(currPage - 1);
+        setOffset(limit * (currPage - 2));
+        handleClick();
+      }
     }
-    clickPrevPage() {
-        const {limit, currPage} = this.state;
-        if (currPage > 1) {
-            this.setState({
-                currPage: currPage - 1,
-                offset: limit * (currPage - 2)
-            }, this.handleClick)
-            // this.$emit('clickPage', {value: this.offset});
-        }
-
-    }
-    clickNextPage() {
-        const {limit, currPage} = this.state;
-        const totalPlay = this.props.data;
-        if (currPage < Math.ceil(totalPlay / limit)) {
-            this.setState({
-                currPage: currPage + 1,
-                offset: limit * (currPage)
-            }, this.handleClick)
-            // this.$emit('clickPage', {value: this.offset});
-        }
-
+    function clickNextPage() {
+      if (currPage < Math.ceil(totalPlay / limit)) {
+        setCurrPage(currPage+1);
+        setOffset(limit * (currPage));
+        handleClick();
+      }
     }
 
-    handleClick() {
-        this.props.onChange(this.state.offset);
+    function handleClick() {
+      props.onChange(offset);
     }
 }
 
